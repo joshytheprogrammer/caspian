0e509551286a691a97da047d33c95f8708e14d6e
Joshua <jurassicjoshua@protonmail.com>
2023-11-29 15:24:34 +0100

    made changes: ''

--- components/App/Nav/Bar.vue
+++ components/App/Nav/Bar.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="w-full bg-white px-8 md:px-12 lg:px-28 py-4 text-neutral-800 flex items-center justify-between">
+  <div class="w-full bg-white px-8 md:px-12 lg:px-28 py-8 text-neutral-800 flex items-center justify-between">
     <div class="w-fit" >
       <NuxtLink class="flex items-center bg-white cursor-pointer text-primary-700" @click.prevent="$router.push('/')" >
-        <img class="w-36" src="/LOGO.png" alt="Caspian Logo">
-        <!-- <span class="text-4xl text-primary-700 font-bold">Caspian</span> -->
+        <!-- <img class="w-36 pr-4" src="/LOGO.png" alt="Caspian Logo"> -->
+        <span class="text-4xl text-primary-700 font-bold">Caspian</span>
       </NuxtLink>
     </div>
     <div class="text-base font-medium no-underline">
-      <NuxtLink class="py-8 px-4 hover:underline hover:decoration-4 decoration-secondary-900 underline-offset-[16px] hover:text-secondary-900 " to="/#">Home</NuxtLink>
-      <NuxtLink class="py-8 px-4 hover:underline hover:decoration-4 decoration-secondary-900 underline-offset-[16px] hover:text-secondary-900" to="/about">About</NuxtLink>
-      <NuxtLink class="py-8 px-4 hover:underline hover:decoration-4 decoration-secondary-900 underline-offset-[16px] hover:text-secondary-900" to="/services">Services</NuxtLink>
-      <NuxtLink class="py-8 px-4 hover:underline hover:decoration-4 decoration-secondary-900 underline-offset-[16px] hover:text-secondary-900" to="/projects">Projects</NuxtLink>
-      <NuxtLink class="py-8 px-4 hover:underline hover:decoration-4 decoration-secondary-900 underline-offset-[16px] hover:text-secondary-900" to="/contact">Contact</NuxtLink>
+      <NuxtLink class="py-8 px-4 hover:underline hover:decoration-4 decoration-primary-900 underline-offset-[16px] hover:text-primary-900 " to="/#">Home</NuxtLink>
+      <NuxtLink class="py-8 px-4 hover:underline hover:decoration-4 decoration-primary-900 underline-offset-[16px] hover:text-primary-900" to="/about">About</NuxtLink>
+      <NuxtLink class="py-8 px-4 hover:underline hover:decoration-4 decoration-primary-900 underline-offset-[16px] hover:text-primary-900" to="/services">Services</NuxtLink>
+      <NuxtLink class="py-8 px-4 hover:underline hover:decoration-4 decoration-primary-900 underline-offset-[16px] hover:text-primary-900" to="/projects">Projects</NuxtLink>
+      <NuxtLink class="py-8 px-4 hover:underline hover:decoration-4 decoration-primary-900 underline-offset-[16px] hover:text-primary-900" to="/contact">Contact</NuxtLink>
     </div>
   </div>
 </template>
