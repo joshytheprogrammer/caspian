ee5cd50ba4aaef1be7c5bc4d59346d539b374df3
Joshua <jurassicjoshua@protonmail.com>
2023-12-06 15:32:04 +0100

    made changes: ''

---
+++ components/Home/Contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-[url('https://res.cloudinary.com/dsgvwxygr/image/upload/v1701787429/caspian/large-cargo-ship-with-containers-port-generative-ai_saksit.jpg')]bg-top bg-cover bg-primary-950 bg-no-repeat bg-fixed h-[430px] w-full">
+    <div class="bg-[rgba(0,0,0,.6)] text-white flex flex-col justify-center items-center w-full h-full space-y-8">
+      <h2 class="text-2xl md:text-4xl lg:text-6xl font-extrabold">Have a project in mind?</h2>
+      <p class="text-xl font-normal tracking-wider">Do not hesitate to say hello?</p>
+      <NuxtLink to="/contact" class="border text-xl border-white px-12 py-4 cursor-pointer hover:bg-white transition-all duration-300 ease-in  hover:text-black ">Let's talk</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
