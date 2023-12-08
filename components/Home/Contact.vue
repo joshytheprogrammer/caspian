5b27edaee2fb34f026c4f8ad560efc1aa2761c70
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-12-08 14:42:10 +0100

    made changes: ''

--- components/Home/Contact.vue
+++ components/Home/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-[url('https://res.cloudinary.com/dsgvwxygr/image/upload/v1701787429/caspian/large-cargo-ship-with-containers-port-generative-ai_saksit.jpg')] bg-top bg-cover bg-primary-950 bg-no-repeat bg-fixed h-[430px] w-full">
+  <div class="bg-[url('https://res.cloudinary.com/dsgvwxygr/image/upload/v1702041504/caspian/fotmftlmzatt3hn0ejrn.jpg')] bg-top bg-cover bg-primary-950 bg-no-repeat bg-fixed h-[480px] w-full">
     <div class="bg-[rgba(0,0,0,.6)] text-white flex flex-col justify-center items-center w-full h-full space-y-8">
       <h2 class="text-2xl md:text-4xl lg:text-6xl font-extrabold">Have a project in mind?</h2>
       <p class="text-xl font-normal tracking-wider">Do not hesitate to say hello?</p>
