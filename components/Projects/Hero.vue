8e95b6190df157f51e54c403c54e1f05ad6b1fed
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-12-08 14:55:15 +0100

    made changes: ''

---
+++ components/Projects/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full text-center py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-24">
+    <h2 class="text-4xl md:text-6xl lg:text-8xl font-extrabold text-primary-950">
+      Projects
+    </h2>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
