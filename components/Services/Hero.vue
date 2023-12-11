a3d8b3458892ad3fa8e565dc9dad74c285bd6967
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-12-11 11:56:40 +0100

    made changes: ''

---
+++ components/Services/Hero.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="bg-[url('https://res.cloudinary.com/dsgvwxygr/image/upload/v1702291570/caspian/view-port-containers-sunset_srcoai.jpg')] text-white flex items-center py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-24 h-[240px] md:h-[480px] bg-no-repeat bg-cover bg-center">
+  </div>
+</template>
