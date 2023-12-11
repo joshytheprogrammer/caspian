6b38d7273829fdbec11133c5e6d63157b9c9b753
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-12-11 09:50:24 +0100

    made changes: ''

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,19 +14,10 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@formkit/auto-animate',
     'vue3-carousel-nuxt',
-    '@nuxtjs/google-fonts',
     'nuxt-icon',
     '@nuxtjs/device',
     '@vueuse/motion/nuxt'
   ],
-  googleFonts: {
-    families: {
-      Poppins: {
-        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-        ital: [100, 200, 300, 400, 500, 600, 700, 800, 900]
-      },
-    }
-  },
   device: {
     refreshOnResize: true
   }
