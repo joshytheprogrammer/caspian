83f7401aa4185815dce2eb66723bc35bf0cce66c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-12-11 16:50:35 +0100

    made changes: ''

--- components/About/Why.vue
+++ components/About/Why.vue
@@ -1,7 +1,7 @@
 <template>
   <div class=" py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-16">
-    <div class="flex justify-between items-center">
-      <div class="">
+    <div class="block lg:flex justify-between items-center">
+      <div class="py-4">
         <div class="py-12">
           <h2 class="text-4xl md:text-6xl lg:text-8xl font-bold text-primary-950">
             Why <br> Choose Us
@@ -11,7 +11,7 @@
         <h3 class="text-xl md:text-2xl lg:text-3xl font-normal tracking-wider text-primary-900">We Bridge Distances, We Deliver Dreams</h3>
       </div>
       <div class="">
-        <img width="540px" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1702309345/caspian/self-driving-futuristic-freight-truck-deliver-goods-warehouse-city-highway-1000x1000_vcwrcl.jpg" alt="">
+        <img class="py-4" width="540px" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1702309345/caspian/self-driving-futuristic-freight-truck-deliver-goods-warehouse-city-highway-1000x1000_vcwrcl.jpg" alt="">
       </div>
     </div>
     <p class=" py-8 lg:py-16 text-base leading-8 text-slate-500">At Caspian, we are more than just a logistics company - we are the architects of seamless supply chains, the navigators of global trade, and the enablers of business growth. With a passion for innovation and a commitment to excellence, we have been at the forefront of the logistics industry for over a decade. Caspian began as a vision to redefine logistics. Our journey started with a small team and a big idea: to provide comprehensive, customer-centric solutions that transcend borders and challenges. Through dedication and hard work, we have grown into a global logistics leader, connecting businesses and markets across continents. At Caspian, our mission is to be the driving force behind global commerce. We are dedicated to providing innovative, reliable, and tailored logistics solutions that empower businesses to thrive in a dynamic world. With a commitment to excellence and a passion for customer success, we strive to create seamless supply chains, bridge distances, and connect opportunities. Through our expertise, technology-driven approach, and unwavering dedication, we aim to be the preferred partner for businesses seeking to navigate the complexities of logistics with confidence.</p>
