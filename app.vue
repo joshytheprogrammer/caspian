5ecc4f6ab131993a2e302387460501584c2eb1f6
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-12-12 14:04:15 +0100

    made changes: 'fixing layout issues'

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
     <AppNavBar v-show="$device.isDesktopOrTablet" />
     <AppNavMBar v-show="$device.isMobile" />
     <NuxtLayout>
-      <NuxtPage class="mt-24 md:mt-0" />
+      <NuxtPage :class="$device.isMobile ? 'mt-24' : 'mt-0'" />
     </NuxtLayout>
     <AppFooter />
   </div>
